68618e5c743b404b14ee0836b9307b3626377547
Andrii Bohomolov <andrey.bogomoloff@gmail.com>
2018-03-05 03:47:13 +0200

    Chapter 5 - Test select user by uid

--- src/test/java/ua/bohomolov/learningspringboot/dao/FakeDataDaoTest.java
+++ src/test/java/ua/bohomolov/learningspringboot/dao/FakeDataDaoTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import ua.bohomolov.learningspringboot.model.User;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,8 +32,21 @@ public class FakeDataDaoTest {
     }
 
     @Test
-    public void selectUserByUserUid() throws Exception {
+    public void shouldSelectUserByUserUid() throws Exception {
+        UUID annaUserUid = UUID.randomUUID();
+        User annaUser = new User(annaUserUid, "anna", "montana", User.Gender.FEMALE, 30, "anna@gmail.com");
+        fakeDataDao.insertUser(annaUserUid, annaUser);
+        assertThat(fakeDataDao.selectAllUsers()).hasSize(2);
 
+        Optional<User> optionalUser = fakeDataDao.selectUserByUserUid(annaUserUid);
+        assertThat(optionalUser.isPresent()).isTrue();
+        assertThat(optionalUser.get()).isEqualToComparingFieldByField(annaUser);
+    }
+
+    @Test
+    public void shouldNotSelectUserByRandomUserUid() throws Exception {
+        Optional<User> optionalUser = fakeDataDao.selectUserByUserUid(UUID.randomUUID());
+        assertThat(optionalUser.isPresent()).isFalse();
     }
 
     @Test
