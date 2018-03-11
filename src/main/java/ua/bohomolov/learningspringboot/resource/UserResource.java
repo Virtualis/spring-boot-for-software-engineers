b57d7103c4be0cbfd1845d62ef06efc3667f4b2f
Andrii Bohomolov <andrey.bogomoloff@gmail.com>
2018-03-12 00:23:48 +0200

    Chapter 6 - HTTP GET Requests

---
+++ src/main/java/ua/bohomolov/learningspringboot/resource/UserResource.java
@@ -0,0 +1,28 @@
+package ua.bohomolov.learningspringboot.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ua.bohomolov.learningspringboot.model.User;
+import ua.bohomolov.learningspringboot.service.UserService;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET
+    )
+    public List<User> fetchUsers() {
+        return userService.getAllUsers();
+    }
+}
