76044f40fb83beed478375d07f9f470983a969de
Andrii Bohomolov <andrey.bogomoloff@gmail.com>
2018-03-12 00:45:37 +0200

    Chapter 6 - Path variables

--- src/main/java/ua/bohomolov/learningspringboot/resource/UserResource.java
+++ src/main/java/ua/bohomolov/learningspringboot/resource/UserResource.java
@@ -1,6 +1,7 @@
 package ua.bohomolov.learningspringboot.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import ua.bohomolov.learningspringboot.model.User;
 import ua.bohomolov.learningspringboot.service.UserService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(
@@ -27,5 +29,13 @@ public class UserResource {
     )
     public List<User> fetchUsers() {
         return userService.getAllUsers();
+    }
+
+    @RequestMapping(
+            path = "{userUid}",
+            method = RequestMethod.GET
+    )
+    public User fetchUser(@PathVariable(name = "userUid") UUID userUid){
+        return userService.getUser(userUid).get();
     }
 }
